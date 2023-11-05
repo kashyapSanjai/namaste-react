05819ce95df3d9de27e479c47e3b709b70311017
mobcoder-sanjaik <sanjai.kumar@mobcoder.com>
2023-11-05 18:26:25 +0530

    episode 1

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const Title = ()=>{
+        <h2 className="head" tabIndex="5">Namaste React Using jsx</h2>
+}
+// const HeadingComponent = ()=><div id="container">{Title} <h1 className="heading">Namaste React Functional Component</h1></div> //alternate of below code
 
-let heading = React.createElement("h1",{id:'heading',xyz:'abc'},"namaste react in js");
+const HeadingComponent = ()=>{
+        return(<div id="container">
+                {Title}
+                <h1 className="heading">Namaste React Functional Component</h1>
+        </div>)
+}
+
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
+// root.render(<HeadingComponent></HeadingComponent>); //also componect call like this
+// root.render(HeadingComponent()); //also componect call like this
