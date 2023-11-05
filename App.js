ed83c6284474b8e386f48090767679422f17d48f
mobcoder-sanjaik <sanjai.kumar@mobcoder.com>
2023-11-05 17:21:08 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 let heading = React.createElement("h1",{id:'heading',xyz:'abc'},"namaste react in js");
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
